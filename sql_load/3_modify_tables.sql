0177bf57e1b88e30bb07e02b50b09dc83925beb0
Jharana Sapkota <jharanasapkota09@gmail.com>
2025-01-06 23:03:02 -0500

    Upload SQL File

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,17 +1,19 @@
-
-
 COPY company_dim
-FROM 'C:\Users\jhara\Downloads\all_folders\csv_files\company_dim.csv'
+FROM 'C:\Users\jhara\Desktop\Projects\Sql_Project\csv_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'C:\Users\jhara\Downloads\all_folders\csv_files\skills_dim.csv'
+FROM 'C:\Users\jhara\Desktop\Projects\Sql_Project\csv_files\skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'C:\Users\jhara\Downloads\all_folders\csv_files\job_postings_fact.csv'
+FROM 'C:\Users\jhara\Desktop\Projects\Sql_Project\csv_files\job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'C:\Users\jhara\Downloads\all_folders\csv_files\skills_job_dim.csv'
+FROM 'C:\Users\jhara\Desktop\Projects\Sql_Project\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+SELECT *
+FROM company_dim
+LIMIT 10;
